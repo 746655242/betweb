ea173084759b9bb1100c25d55b12dd8cfa9c5b16
nickypeng <nicky_peng@163.com>
2019-10-27 22:37:57 +0800

    修改投注

--- spApp/src/pages/mine/betting.vue
+++ spApp/src/pages/mine/betting.vue
@@ -1,186 +1,50 @@
 <template>
-    <div v-title data-title="投注记录" class="wrapBox">
-
-        <header class="title">      
-			<span onclick="history.go(-1)" class="iconfont icon-fanhui">&#xe614;</span>
-			<span>投注记录</span>
-        </header>
-
-        <tab bar-active-color="#ef1823" v-model="index"  :line-width="3" custom-bar-width="20px" >
-            <tab-item selected @on-item-click="onItemClick">全部投注 </tab-item>
-            <tab-item @on-item-click="onItemClick">最近中奖</tab-item>
-        </tab>
-
-
-        <div class="ls" ref='ls'>
-
-            <div v-if="index==0" class="ul  tab-swiper vux-center"  >
-                <a v-for="(item,index) in listdata"  @click="betinfoSw(item)" :key="index" class="item">  
-                    <img class="pic" :src="base.lotteryData[item.game_type].IconPic" >
-                    <p class="t1">
-                        <span v-if='[1,5].indexOf(item.game_type)>=0'>{{base.lotteryData[item.game_type].IconName}} {{item.bet_con.split(',').length}}串1@{{item.odds}} </span>
-                        <span v-else  >{{base.lotteryData[item.game_type].IconName}} 第{{item.issue}}期 </span>
-
-                        <span class="fr">{{base.statuss[item.status]}}</span>
-                    </p>
-                    <p class="c1"><span>{{item.date}}</span><span class="fr">{{item.bet_amount}}元</span></p> 
-                </a>
-                <div class="btnmoer" @click="tabindex">查看更多</div>
-            </div>
-
-            
-        
-            <div v-if="index==1" class="ul  tab-swiper vux-center" >
-                <a v-for="(item,index) in ylistdata"  @click="betinfoSw(item)" :key="index" class="item">  
-                    <img class="pic" :src="base.lotteryData[item.game_type].IconPic" >
-                    <p class="t1">
-                        
-                         <span v-if='[1,5].indexOf(item.game_type)>=0'>{{base.lotteryData[item.game_type].IconName}} {{item.bet_con.split(',').length}}串1@{{item.odds}} </span>
-                         <span v-else  >{{base.lotteryData[item.game_type].IconName}} 第{{item.issue}}期 </span>
-
-
-                        
-                        <span class="fr">{{base.statuss[item.status]}}</span>
-                    </p>
-                    <p class="c1"><span>{{item.date}}</span><span class="fr">{{item.bet_amount}}元</span></p>     
-                </a>
-                <div class="btnmoer" @click="ytabindex">查看更多</div>
-            </div>
-                
-        
-
-
+<div>
+    <link href="./static/css/user.css" rel="stylesheet">
+    <link href="./static/css/dropload.css" rel="stylesheet">
+    <header class="head headfixed">
+        <div class="back w60"><a class="backlink"></a></div>
+        <div class="headertit">
+        <nav class="flexbox headermenu">
+            <p class="boxflex border_r" @click="onItemClick(0)"><a v-bind:class="{'sed':index==0}">全部投注</a></p>
+            <p class="boxflex border_r" @click="onItemClick(1)"><a  v-bind:class="{'sed':index==1}">最近中奖</a></p>
+        </nav>
         </div>
-
-
-        <div class="betinfo" :class="{'betshow':betinfoData.betList}"  >
-            <div class="head">
-                    <span @click="colsebetinfoSw" class="iconfont icon-fanhui">&#xe614;</span>
-                    <span>投注记录</span>
-            </div>
-            <div class="main">
-
-                <div class="betcom">
-
-                    <div class="bettxt">
-                        投注编号：{{betinfoData.bet_id}}<br>
-                        投注时间：{{betinfoData.date}}
-                    </div>
-
-
-
-                    <div v-if='betinfoData.game_type==3||betinfoData.game_type==4'>
-                        <table>
-                            <tr>
-                                
-                                <th>项目</th>
-                                <th>投注项</th>
-                            </tr>
-                            <tr>
-                              
-                                <td>{{base.lotteryData[betinfoData.game_type].IconName}}</td>
-                                <td width="70%" class="sfccom">
-                                    <div>
-                                        <span v-for="(item,key) in  betinfoData.bet_con.split(',') " 
-                                        :key="key" >{{item.split('|')[0]}}<br><i>{{item.split('|')[1]}}</i></span>
-                                      
-                                    </div>
-                                </td>
-                                
-                            </tr>
-                            </table>
-
-                             <table>
-                            <tr>    
-                                <th>期号</th>
-                                <th>投注额</th>
-                                <th>奖金</th>
-                                <th>结果</th>
-                            </tr>
-                            <tr>
-                                <td>{{betinfoData.issue}}</td>
-                                <td>{{betinfoData.bet_amount}}</td>
-                                <td>{{betinfoData.status==1?betinfoData.bonus:0}}元</td>
-                                <td>{{base.statuss[betinfoData.status]}}</td>
-                            </tr>
-                        </table>
-                        <div class="bettxt" v-if="betinfoData.betList[0]" >
-                            <p>足彩胜负/任九 {{ betinfoData.betList[0]['issue']}}期</p>
-                            
-                            <span v-for="(ite,index) in betinfoData.betList[0]['gameres'].split(',')" :key="index"> {{ite}} </span>
-                            
-    
-                        </div>
-
-                        <table v-if="betinfoData.betList[0]" >
-                            <tr>
-                                <th>玩法</th>
-                                <th>奖项</th>
-                                <th>注数</th>
-                                <th>单注金额(元)</th>
-                            </tr>
-                            <tr>
-                                <td rowspan="2" >胜负</td>
-                                <td>一等奖</td>
-                                <td>{{ betinfoData.betList[0]['num14']}}</td>
-                                <td>{{ betinfoData.betList[0]['bonus14']}}</td> 
-                            </tr>
-                            <tr>
-                                <td>二等奖</td>
-                                <td>{{ betinfoData.betList[0]['num13']}}</td>
-                                <td>{{ betinfoData.betList[0]['bonus13']}}</td> 
-                            </tr>
-                            <tr>
-                                <td>任9</td>
-                                <td>一等奖</td>
-                                <td>{{ betinfoData.betList[0]['num9']}}</td>
-                                <td>{{ betinfoData.betList[0]['bonus9']}}</td> 
-                            </tr>
-                        </table>
-
-
-                    </div>
-
-                    <div v-else>
-                        <table>
-                            <tr>
-                                <th>序号项</th>
-                                <th>投注项</th>
-                                <th>赛事</th>
-                                <th>赔率</th>
-                                <th>结果</th>
-                            </tr>
-                            <tr v-for="(item,index) in betinfoData.betList" :key="index">
-                                <td>{{index}}</td>
-                                <td>{{item.bet_com}}</td>
-                                <td>{{item.game_name}}</td>
-                                <td>{{item.odds}}</td>
-                                <td>{{base.statuss[item.status]}}</td>
-                            </tr>
-                        </table>
-
-                        <table>
-                            <tr>
-                                <th width="25%">投注类型</th>
-                                <th width="25%">投注金额</th>
-                                <th width="25%">返还</th>
-                                <th width="25%">结果</th>
-                            </tr>
-                            <tr>
-                                <td>{{betinfoData.betList&&betinfoData.betList.length}}串1</td>
-                                <td>{{betinfoData.bet_amount}}元</td>
-                                <td>{{betinfoData.status==1?betinfoData.bonus:0}}元</td>
-                                <td>{{base.statuss[betinfoData.status]}}</td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
+        <div class="headBtnbox topcenter w60"><a><em class="headicon icon_screen"></em></a></div>
+    </header>
+    <div class="pt48" id="dropload_body">
+    <div class="betlisttit gray8d textc bgf5">
+      <!-- <p>日期</p> -->
     </div>
 
+    <section class="betlist font12">
+      <div class="nulllotteryno" v-if="listdata.length<1">
+        <p class="nulllotterynoimg"><img src="~@/assets2/images/nulllotteryno-83be1b1b40.png" alt=""></p>
+        <p class="font14 gray5">当前无记录</p>
+      </div>
+      <ul class="listboxplr0" v-if="listdata.length>0">
+        <li class="list_item_dashed relative" v-for="(item,index) in listdata" :key="index">
+            <a class="topcenter gray8b">
+                <!-- <em class="identifier_gendan" style="display: none;"> </em> -->
+                <div class="listdata mb1">
+                <p class="day">{{item.day}}</p>
+                <p class="month">{{item.month}}月</p>
+                </div>
+                <div class="boxflex"><span class="mb10 doingbg">{{item.status_title}}</span></div>
+                <div class="icon_activity activityicon" style="display: none;"></div>
+                <div class="textr gray8d ">
+                <p><span class="font10 caitype">{{item.game_type_title}}</span></p>
+                <!-- <p class="plannum"><span class="font10">9920</span></p> -->
+                <p class="moneytext">{{item.bet_amount}}</p>
+                </div>
+            </a>
+        </li>
+      </ul>
+
+    </section>
+    <div class="dropload-down"></div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -207,9 +71,7 @@
         },
         created(){
             this.fetchData();
-            this.ygetdata();
-
-           
+            // this.ygetdata();
         },
 		computed:{
 			...mapGetters({
@@ -227,7 +89,6 @@
             
         },
         methods: {
-            
         	fetchData(){
 				let me= this;
                 let user=this.user.info
@@ -243,7 +104,6 @@
                         }
                     }).catch(function(err){
                         console.log(err)
-                        
                     })
         		}else{
 					this.$router.push('/login')
@@ -251,7 +111,6 @@
             },
             onItemClick(index){
                 this.index=index;
-               
             },
             tabindex(){
                 if(this.page+1<=this.totalpage){
@@ -278,7 +137,6 @@
                         }
                     }).catch(function(err){
                         console.log(err)
-                        
                     })
         		}else{
 					this.$router.push('/login')
@@ -294,13 +152,10 @@
                 }
             },
             
-
             //显示详情
             betinfoSw(item){
-               
-                
                 if([3,4].indexOf(item['game_type']) >= 0){
-                    var  ids=item.issue;
+                    var ids=item.issue;
                 }else if([1,5].indexOf(item['game_type']) >= 0 ){
                     let betCom=item.bet_con.split(',');
                     var  ids=[];
@@ -333,159 +188,38 @@
                 })
             },
 
-
             colsebetinfoSw(){
                 this.betinfoData={};
             }
-            
-
         }
-       
     }
 </script>
 
 <style lang="less" scoped="scoped">
-
-    .wrapBox{
-        padding-top:.44rem;
-    }
-    header{
-        background: #fff !important;
-        color:#333;
-    }
-
-    .ls{
-        overflow: auto;
-        position: absolute;
-        left: 0;
-        right: 0;
-        bottom:0;
-        top:1rem;
-       
-        .item{
-            background: #fff;
-            display: block;
-        
-            padding:.15rem .2rem .15rem .6rem;
-            position: relative;
-            border-bottom:1px solid #efeff4;
-            .pic{
-                position:absolute;
-                left:.1rem;
-                top:.15rem;
-                width: .36rem;
-            }
-            .t1{
-                font-size:.14rem;
-                color:#333;
-            }
-            .c1{
-                margin-top:.05rem;
-                font-size:.1rem;
-                color:#999;
-            }
-            .fr{
-                float: right;
-            }
-        }
-        .btnmoer{
-            background: #fff;
-            text-align: center;
-            padding:.15rem 0;
-            color:#999;
-        }
-    }
-
-    .betinfo{
-        position: fixed;
-        top:0;
-        bottom:0;
-        left:100%;
-        width: 100%;
-        right:0;
-        background: #f3f3f3;
-        z-index: 999;
-        transition: all .5s;  
-        .head{
-            background: #ef1823 !important;
-            height: .5rem;
-            color:#fff;
-            font-size:.2rem;
-            line-height: .5rem;
-            text-align: center;
-            .icon-fanhui{
-                font-size:.3rem;
-                position: absolute;
-                left:0;
-                padding: 0 .05rem;
-            }
-        }
-        .main{
-            top:.5rem;
-            bottom: 0;
-            overflow: auto;
-            .betcom{
-                margin: .1rem;
-                background: #fff;
-                padding:.1rem;
-                color:#333;
-                font-size:.14rem;
-                border-radius: .05rem;
-                box-shadow: 0 3px 5px rgba(0,0,0,.05);
-                .bettxt{
-                    line-height: 1.6;
-                    span{
-                        display: inline-block;
-                        background: #093;
-                        color:#fff;
-                        font-size:.16rem;
-                        padding:0 .02rem;
-                        margin:0 .01rem;
-                    }
-                    p{
-                        margin-bottom:.05rem;
-                    }
-                }
-                
-                table{
-                    width:100% ;
-                    margin:.1rem 0;
-                    td,th{
-                         border:1px solid #e0e0e0;
-                         background: #fff;
-                         text-align: center;
-                         line-height: .4rem;
-                    }
-                    .sfccom{
-                        div{
-                            font-size:0;
-                            padding-top:.1rem;
-                            span{
-                                line-height: .2rem;
-                                font-size:.1rem;
-                               
-                                margin:0 1px;
-                                display:inline-block;
-                                i{
-                                    background: #093;
-                                    color:#fff;
-                                    padding:0 .02rem;
-                                    font-size:16px;
-                                }
-                            }
-                        }
-                    }
-                    th{
-                        background: #f3f3f3;
-                    }
-                   
-                }
-
-            }
-        }
-    }
-    .betshow{
-        left:0;
-    }
-
+.red{
+    color: #F76665 !important;
+}
+.bgHeader{
+    position: fixed;
+    bottom:0;
+    left:0;
+    top:0;
+    right:0;
+    background: rgba(0,0,0,.5);
+    z-index: 60;
+}
+.bg{
+    position: fixed;
+    bottom:0;
+    left:0;
+    top:0;
+    right:0;
+    background: rgba(0,0,0,.5);
+    z-index: 69;
+}
+.beton{
+    background: #ef1823!important;
+    color:#fff !important;
+    span{color:#fff !important;}
+}
 </style>
